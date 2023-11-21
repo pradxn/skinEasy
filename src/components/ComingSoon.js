c888f1ee2ebb5b44b148f272d83484e0c7060995
Pankaj Naik <pankajnaik@Pankajs-MacBook-Air.local>
2023-11-21 12:33:14 +0530

    TestFooter and ChangingFunction fixes

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './ComingSoon.css';
-
+import ChatBot from './ChatBot';
+import iconImage from '../images/face1.jpg';
 const ComingSoon = () => {
   return (
-    <div className="coming-soon-container">
-      <h1 className="coming-soon-title">Coming Soon</h1>
-      <p className="coming-soon-description">We are working on something amazing for you!</p>
-    </div>
+    <>
+      <div className="coming-soon-container">
+        <h1 className="coming-soon-title">Coming Soon</h1>
+        <p className="coming-soon-description">We are working on something amazing for you!</p>
+      </div>
+      <div>
+        <ChatBot icon={iconImage} />
+      </div>
+    </>
   );
 };
 
--- src/components/Footer/TermsFooter.js
+++ src/components/Footer/TermsFooter.js
@@ -7,11 +7,11 @@ function TermsFooter() {
       <div className="footer-line"></div> {/* Line above */}
       <div className="terms-footer">
         <div className="tagline">
-          <p style={{ whiteSpace: "nowrap" }}>
+          <div style={{ whiteSpace: "nowrap" }}>
             {/*Check your products for purity. Throw those faulty & fake
             products. Find out the reality of those "organic" products.*/}
             Seba: Elevating Skincare Choices Through In-Depth Ingredient Analysis for Lasting Skin Wellness
-          </p>
+          </div>
           <br />
         </div>
 
@@ -20,17 +20,17 @@ function TermsFooter() {
         <div className="links-row">
           <div className="column">
             <span className="column-title">Get to know us</span>
-            <a href="/about"  rel="noreferrer">
+            <a href="/about" rel="noreferrer">
               About</a>
             <br />
-            <a href="/grading-method"  rel="noreferrer">
+            <a href="/grading-method" rel="noreferrer">
               How we grade products</a>
             <br />
-            <a href="mailto:consult@toxicfree.com"  rel="noreferrer">
+            <a href="mailto:consult@toxicfree.com" rel="noreferrer">
               Email Us
             </a>
             <br />
-            <a href="mailto:influence.marketing@toxicfree.com"  rel="noreferrer">
+            <a href="mailto:influence.marketing@toxicfree.com" rel="noreferrer">
               PR Marketing
             </a>
             <br />
@@ -38,28 +38,28 @@ function TermsFooter() {
 
           <div className="column">
             <span className="column-title">Services</span>
-            <a href="/consult"  rel="noreferrer">
+            <a href="/consult" rel="noreferrer">
               Consult a dermatologist</a>
             <br />
-            <a href="/home-consult"  rel="noreferrer">
+            <a href="/home-consult" rel="noreferrer">
               Home consultation</a>
             <br />
-            <a href="/shop"  rel="noreferrer">
+            <a href="/shop" rel="noreferrer">
               Shop</a>
-              <br />
+            <br />
           </div>
           <div className="column">
             <span className="column-title">Links</span>
-            <a href="/api-ref"  rel="noreferrer">
+            <a href="/api-ref" rel="noreferrer">
               API</a>
             <br />
-            <a href="/business"  rel="noreferrer">
+            <a href="/business" rel="noreferrer">
               Business</a>
             <br />
-            <a href="/sitemap"  rel="noreferrer">
+            <a href="/sitemap" rel="noreferrer">
               Sitemap</a>
             <br />
-            <a href="/blog"  rel="noreferrer">
+            <a href="/blog" rel="noreferrer">
               Blog</a>
             <br />
           </div>
